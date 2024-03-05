eca2ac3dea2436bd8757ce66b0b402d5d5acb823
Etukudoh Marvellous <Marvellous Etukudoh>
2024-03-05 04:21:13 +0100

    created new Psql and redis instance and configured them on the env file and env template

--- medusa-config.js
+++ medusa-config.js
@@ -121,6 +121,11 @@ const projectConfig = {
 module.exports = {
   projectConfig:{
     //...
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
     database_extra: process.env.NODE_env !== "development" ?
     {
       ssl: {
